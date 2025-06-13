6a480f50a933a80abad231534b015a65433e0615
Sugar <140928.miina@gmail.com>
2025-06-13 20:37:56 +0900

    fixed button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,9 +2,8 @@ import ボタン from "../assets/ボタン.png";
 
 const Button: React.FC = () => {
   return (
-    <div className="w-[282px] h-[73px]" data-model-id="1010:201-frame">
+    <div className="fixed w-[282px] h-[73px]">
       <img
-        className="fixed w-[294px] h-[85px] top-0 left-0"
         alt="Image"
         src={ボタン}
       />
--- src/screens/LandingPage.tsx
+++ src/screens/LandingPage.tsx
@@ -4,13 +4,13 @@ import Button from "../components/Button";
 function LandingPage() {
   return (
     <>
-      <div className="flex flex-col">
-        <img
-          src={確定画像}
-          alt="キャンペーンバナー"
-          className="w-full max-w-[400px]"
-        />
-        <Button />
+      <div className="flex flex-col items-center">
+        <div className="relative w-full max-w-[400px]">
+          <img src={確定画像} alt="バナー" className="w-full" />
+          <div className="absolute top-115 left-4 w-[90%] flex justify-center">
+            <Button />
+          </div>
+        </div>
       </div>
     </>
   );
