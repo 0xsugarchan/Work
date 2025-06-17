374e05aa0953ca8f86efedec7ce3193aa25bfdee
Sugar <140928.miina@gmail.com>
2025-06-17 19:55:12 +0900

    Add Footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,24 +1,18 @@
 import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
-	return (
-		<footer className="bg-[#343434] text-white text-sm">
-			<div className="flex justify-center gap-8 py-4 [font-family:'Noto_Sans_JP-Regular',Helvetica]">
-				<Link to="/" className="hover:underline">
-					TOP
-				</Link>
-				<Link to="/privacy-policy" className="hover:underline">
-					プライバシーポリシー
-				</Link>
-				<Link to="/terms" className="hover:underline">
-					利用規約
-				</Link>
-			</div>
-			<div className="bg-[#141414] text-center py-2 text-sm [font-family:'Geologica-Regular',Helvetica]">
-				©2025 ankh agency
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="bg-[#343434] text-white text-xs sm:text-xs md:text-lg">
+      <div className="flex justify-center gap-4 py-6 [font-family:'Noto_Sans_JP-Regular',Helvetica] sm:gap-2 sm:py-4">
+        <Link to="/">TOP</Link>
+        <Link to="/privacy-policy">プライバシーポリシー</Link>
+        <Link to="/terms">利用規約</Link>
+      </div>
+      <div className="bg-[#141414] text-center py-1 text-xs [font-family:'Geologica-Regular',Helvetica]">
+        ©2025 ankh agency
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
