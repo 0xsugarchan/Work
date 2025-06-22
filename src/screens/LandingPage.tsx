14a37cdfdfb65a524f55f07a91cade26be5f6b05
Sugar <140928.miina@gmail.com>
2025-06-22 21:28:22 +0900

    Adjust Layout Shift

--- src/screens/LandingPage.tsx
+++ src/screens/LandingPage.tsx
@@ -13,88 +13,85 @@ function LandingPage() {
   const { heroRef, isFixed } = useLandingPage();
 
   return (
-    <div className="flex flex-col items-center">
-      <div ref={heroRef} className="relative w-full">
-        <img
-          src={heroImg}
-          alt="heroImg"
-          loading="lazy"
-          width={390}
-          height={550}
-          className="w-full block"
-        />
-        <div
-          className={`${
-            isFixed ? "fixed bottom-[20%]" : "absolute top-[78%] md:top-[80%]"
-          } left-1/2 -translate-x-1/2 z-50 w-[80%] sm:w-[60%] md:w-[40%] lg:w-[50%]
-          transition-all duration-500 ease-in-out
-    ${isFixed ? "opacity-100 translate-y-0" : "opacity-100 translate-y-4"}
-`}
-        >
-          <Button />
+    <div className="w-full flex justify-center">
+      <div className="w-full max-w-[390] px-4">
+        <div ref={heroRef} className="relative">
+          <img
+            src={heroImg}
+            alt="heroImg"
+            loading="lazy"
+            width={390}
+            height={550}
+            className="w-full h-auto block"
+          />
+          <div
+            className={`${
+              isFixed ? "fixed bottom-[20%]" : "absolute top-[78%] md:top-[80%]"
+            } left-1/2 -translate-x-1/2 z-50
+              w-[280px] h-[60px] md:w-[320px] md:h-[64px]
+              transition-all duration-500 ease-in-out
+              ${
+                isFixed
+                  ? "opacity-100 translate-y-0"
+                  : "opacity-100 translate-y-4"
+              }
+            `}
+          >
+            <Button />
+          </div>
         </div>
-      </div>
-      <div className="w-full">
         <img
           src={concernCommitImg}
           alt="concernCommitImg"
           loading="lazy"
           width={390}
           height={1176}
-          className="w-full block"
+          className="w-full h-auto block"
         />
-      </div>
-      <div className="w-full bg-white pt-5">
-        <img
-          src={jobRevenueImg}
-          alt="jobRevenue"
-          loading="lazy"
-          width={390}
-          height={550}
-          className="w-full block"
-        />
-      </div>
-      <div className="w-full">
+        <div className="bg-white pt-5">
+          <img
+            src={jobRevenueImg}
+            alt="jobRevenue"
+            loading="lazy"
+            width={390}
+            height={550}
+            className="w-full h-auto block"
+          />
+        </div>
         <img
           src={stepImg}
           alt="stepImg"
           loading="lazy"
           width={390}
           height={446}
-          className="w-full block"
+          className="w-full h-auto block"
         />
-      </div>
-      <div className="w-full">
         <img
           src={reviewImg}
           alt="reviewImg"
           loading="lazy"
           width={390}
           height={625}
-          className="w-full block"
+          className="w-full h-auto block"
         />
-      </div>
-      <div className="w-full">
         <img
           src={letsBeginImg}
           alt="letsBegin"
           loading="lazy"
           width={390}
           height={505}
-          className="w-full block"
+          className="w-full h-auto block"
         />
-      </div>
-      <div className="w-full">
         <img
           src={aboutCompanyImg}
           alt="aboutCompany"
           loading="lazy"
           width={390}
           height={250}
-          className="w-full block"
+          className="w-full h-auto block"
         />
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
