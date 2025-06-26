bb2905e02673ff822f4d4cb997653c0aac8fa1b9
Sugar <140928.miina@gmail.com>
2025-06-26 18:51:17 +0900

    Changed button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,17 +1,17 @@
 import button_img from "../../assets/line-button.png";
 
 const Button: React.FC = () => {
-  return (
-    <div className="w-full mx-auto">
-      <a
-        href="https://s.lmes.jp/landing-qr/2007556903-QWyp2W5v?uLand=IGMpMv"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img alt="Line Button" src={button_img} className="w-full" />
-      </a>
-    </div>
-  );
+	return (
+		<div className="w-full mx-auto">
+			<a
+				href="https://s.lmes.jp/landing-qr/2007556903-QWyp2W5v?uLand=IGMpMv"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<img alt="Line Button" src={button_img} className="w-full" />
+			</a>
+		</div>
+	);
 };
 
 export default Button;
