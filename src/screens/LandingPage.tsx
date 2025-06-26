bc46040e26402103f890e3e68dcc3aefe5cb00e2
Sugar <140928.miina@gmail.com>
2025-06-26 18:52:09 +0900

    Changed button

--- src/screens/LandingPage.tsx
+++ src/screens/LandingPage.tsx
@@ -10,79 +10,79 @@ import Footer from "../components/ui/Footer";
 import bannerIng from "../assets/banner.png";
 
 function LandingPage() {
-	return (
-		<div className="w-full flex justify-center">
-			<div className="w-full sm:max-w-[390px] mx-auto">
-				<img
-					src={heroImg}
-					alt="heroImg"
-					loading="eager"
-					width={390}
-					height={540}
-					className="w-full h-auto block"
-				/>
-				<img
-					src={concernCommitImg}
-					alt="concernCommitImg"
-					loading="lazy"
-					width={425}
-					height={1176}
-					className="w-full h-auto block"
-				/>
-				<div className="fixed bottom-[3%] left-1/2 -translate-x-1/2 z-50 w-full h-auto block sm:max-w-[390px]">
-					<div className="relative">
-						<img alt="Banner" src={bannerIng} className="w-full" />
-						<div className="absolute bottom-4 right-3 w-[53%] max-w-[210px]">
-							<Button />
-						</div>
-					</div>
-				</div>
-				<div className="bg-white pt-5">
-					<img
-						src={jobRevenueImg}
-						alt="jobRevenue"
-						loading="lazy"
-						width={391}
-						height={550}
-						className="w-full h-auto block"
-					/>
-				</div>
-				<img
-					src={stepImg}
-					alt="stepImg"
-					loading="lazy"
-					width={390}
-					height={446}
-					className="w-full h-auto block"
-				/>
-				<img
-					src={reviewImg}
-					alt="reviewImg"
-					loading="lazy"
-					width={390}
-					height={625}
-					className="w-full h-auto block"
-				/>
-				<img
-					src={letsBeginImg}
-					alt="letsBegin"
-					loading="lazy"
-					width={392}
-					height={505}
-					className="w-full h-auto block"
-				/>
-				<img
-					src={aboutCompanyImg}
-					alt="aboutCompany"
-					loading="lazy"
-					width={390}
-					height={250}
-					className="w-full h-auto block"
-				/>
-				<Footer />
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full flex justify-center">
+      <div className="w-full sm:max-w-[390px] mx-auto">
+        <img
+          src={heroImg}
+          alt="heroImg"
+          loading="eager"
+          width={390}
+          height={540}
+          className="w-full h-auto block"
+        />
+        <img
+          src={concernCommitImg}
+          alt="concernCommitImg"
+          loading="lazy"
+          width={425}
+          height={1176}
+          className="w-full h-auto block"
+        />
+        <div className="fixed bottom-[1%] left-1/2 -translate-x-1/2 z-50 w-full h-auto block sm:max-w-[390px]">
+          <div className="relative">
+            <img alt="Banner" src={bannerIng} className="w-full" />
+            <div className="absolute bottom-4 right-3 w-[53%] max-w-[210px]">
+              <Button />
+            </div>
+          </div>
+        </div>
+        <div className="bg-white pt-5">
+          <img
+            src={jobRevenueImg}
+            alt="jobRevenue"
+            loading="lazy"
+            width={391}
+            height={550}
+            className="w-full h-auto block"
+          />
+        </div>
+        <img
+          src={stepImg}
+          alt="stepImg"
+          loading="lazy"
+          width={390}
+          height={446}
+          className="w-full h-auto block"
+        />
+        <img
+          src={reviewImg}
+          alt="reviewImg"
+          loading="lazy"
+          width={390}
+          height={625}
+          className="w-full h-auto block"
+        />
+        <img
+          src={letsBeginImg}
+          alt="letsBegin"
+          loading="lazy"
+          width={392}
+          height={505}
+          className="w-full h-auto block"
+        />
+        <img
+          src={aboutCompanyImg}
+          alt="aboutCompany"
+          loading="lazy"
+          width={390}
+          height={250}
+          className="w-full h-auto block"
+        />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
